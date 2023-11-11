dfe88b2be707180ac7967fa333914623e29e8834
saov4000 <saov4000@gmail.com>
2023-11-11 12:16:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+function sorteard4(){
+    var n = Math.floor(Math.random()*4)+1
+    document.getElementById("d4").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard6(){
+    var n = Math.floor(Math.random()*6)+1
+    document.getElementById("d6").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard10(){
+    var n = Math.floor(Math.random()*10)+1
+    document.getElementById("d10").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard12(){
+    var n = Math.floor(Math.random()*12)+1
+    document.getElementById("d12").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard16(){
+    var n = Math.floor(Math.random()*16)+1
+    document.getElementById("d16").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard20(){
+    var n = Math.floor(Math.random()*20)+1
+    document.getElementById("d20").innerHTML = n
+    document.getElementById("dtodos").innerHTML = n
+}
+
+function sorteard100(){
+    var n = Math.floor(Math.random()*11)
+    document.getElementById("d100").innerHTML = n.toString()+"0"
+    document.getElementById("dtodos").innerHTML = n.toString()+"0" 
+}
+
+
+
+
+
+
